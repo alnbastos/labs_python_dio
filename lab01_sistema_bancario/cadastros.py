c57903e7c8c77b17e356dca602bb737721d25135
alnbastos <alinebastos23@outlook.com>
2025-11-24 17:19:41 -0300

    refactor: reorganiza e corrige funções

--- lab01_sistema_bancario/cadastros.py
+++ lab01_sistema_bancario/cadastros.py
@@ -1,43 +1,79 @@
 import re
+import textwrap
 
 
-def criar_cliente(nome: str, data_nascimento: str, cpf: str, endereco: str) -> dict:
+def filtrar_cliente(cpf: str, clientes: list[dict]):
+    """Filtra cliente pelo cpf. Evita duplicidade."""
+    cliente = [cliente for cliente in clientes if cliente.get('cpf') == cpf]
+    return cliente[0] if cliente else None
+
+
+def criar_cliente(clientes: list) -> None:
+
     def validar_cpf(cpf: str):
         """Validar CPF, apenas números."""
         padrao = r"^\d{11}$"
         return bool(re.match(padrao, cpf))
 
-    if not nome:
-        print('Operação falhou! O nome é obrigatório, por favor preencha.')
-        return {}
+    cpf = str(input('Informe o CPF (somente números): '))
 
     if not validar_cpf(cpf):
-        print('Operação falhou! Preencha o CPF apenas com números.')
-        return {}
+        print('@@@ Operação falhou! Preencha o CPF apenas com números. @@@')
+        return
 
-    return {
+    if filtrar_cliente(cpf, clientes):
+        print('@@@ Operação falhou! Este CPF já foi cadastrado. @@@')
+        return
+
+    nome = str(input('Informe o nome: '))
+    if not nome:
+        print('@@@ Operação falhou!'
+              ' O nome é obrigatório, por favor preencha. @@@')
+        return
+
+    data_nascimento = str(
+        input('Informe a data de nascimento: ')
+    )
+    endereco = str(
+        input('Informe o endereço (logradouro, nro - bairro - cidade/uf): ')
+    )
+
+    clientes.append({
         'nome': nome,
         'data_nascimento': data_nascimento,
         'cpf': cpf,
         'endereco': endereco,
-    }
+    })
+
+    print('\n=== Cliente criado com sucesso! ===\n')
 
 
-def criar_conta_corrente(contas_correntes: list[dict], clientes: list[dict], cpf_cliente: str):
-    agencia = '0001'
-    numero_conta = (
-        max([cc.get('numero_conta') for cc in contas_correntes]) + 1
-        if contas_correntes
-        else 1
-    )
-    cliente = [c.get('nome') for c in clientes if c.get('cpf') == cpf_cliente]
+def criar_conta_corrente(
+    agencia: str, contas_correntes: list[dict], clientes: list[dict]
+) -> None:
+    cpf_cliente = str(input('Informe o CPF do cliente: '))
+    numero_conta = len(contas_correntes) + 1
+    cliente = filtrar_cliente(cpf_cliente, clientes)
 
     if not cliente:
-        print('Operação falhou! Cliente não encontrado.')
-        return {}
+        print('@@@ Operação falhou! Cliente não encontrado. @@@')
+        return
 
-    return {
+    contas_correntes.append({
         'agencia': agencia,
         'numero_conta': numero_conta,
         'cliente': cliente,
-    }
+    })
+
+    print('\n=== Conta Corrente criado com sucesso! ===\n')
+
+
+def listar_contas_corrente(contas_corrente: list[dict]):
+    for cc in contas_corrente:
+        linha = f"""\
+            Agência:\t{cc.get('agencia')}
+            C/C:\t\t{cc.get('numero_conta')}
+            Titular:\t{cc.get('cliente').get('nome')}
+        """
+        print('='*40)
+        print(textwrap.dedent(linha))
--- lab01_sistema_bancario/main.py
+++ lab01_sistema_bancario/main.py
@@ -1,85 +1,76 @@
-from cadastros import criar_cliente, criar_conta_corrente
-from operacoes_bancarias import depositar, sacar, exibir_extrato
+import textwrap
 
-menu = '''
-
-[u] Criar cliente
-[c] Criar conta corrente
-[d] Depositar
-[s] Sacar
-[e] Extrato
-[q] Sair
-
-=> '''
-
-saldo = 0
-limite = 500
-extrato = ''
-numero_saques = 0
-LIMITE_SAQUES = 3
-clientes: list[dict] = []
-contas_correntes: list[dict] = []
+from cadastros import (
+    criar_cliente, criar_conta_corrente, listar_contas_corrente
+)
+from operacoes_bancarias import (
+    depositar, sacar, exibir_extrato
+)
 
 
-while True:
+def menu():
+    menu = '''
+    ================ MENU ================
+    [d]\tDepositar
+    [s]\tSacar
+    [e]\tExtrato
+    [nc]\tNovo cliente
+    [ncc]\tNova conta corrente
+    [lc]\tListar conta corrente
 
-    opcao = input(menu)
+    [q]\tSair
 
-    if opcao == 'u':
-        print('\n================ CADASTRO DO CLIENTE ================')
+    => '''
+    return input(textwrap.dedent(menu))
 
-        nome = str(input('Informe o nome: '))
-        data_nascimento = str(input('Informe a data de nascimento: '))
-        cpf = str(input('Informe o CPF: '))
-        endereco = str(input('Informe o endereço: '))
 
-        if any([cliente.get('cpf') == cpf for cliente in clientes]):
-            print('Operação falhou! Este CPF já foi cadastrado.')
-        else:
-            cliente = criar_cliente(nome, data_nascimento, cpf, endereco)
-            if cliente:
-                clientes.append(cliente)
+def main():
+    LIMITE_SAQUES = 3
+    AGENCIA = '0001'
 
-        print('==========================================')
+    saldo = 0
+    limite = 500
+    extrato = ''
+    numero_saques = 0
+    clientes: list[dict] = []
+    contas_correntes: list[dict] = []
 
-    elif opcao == 'c':
-        print('\n================ CADASTRO DE CONTA CORRENTE ================')
+    while True:
 
-        cpf_cliente = str(input('Informe o CPF do cliente que a conta será vinculada: '))
-        cc = criar_conta_corrente(contas_correntes, clientes, cpf_cliente)
-        if cc:
-            contas_correntes.append(cc)
+        opcao = menu()
 
-        print('==========================================')
+        if opcao == 'nc':
+            criar_cliente(clientes)
 
-    elif opcao == 'd':
-        valor = float(input('Informe o valor do depósito: '))
-        novo_saldo, novo_extrato = depositar(saldo, valor, extrato)
-        if novo_saldo and novo_extrato:
-            saldo, extrato = novo_saldo, novo_extrato
+        elif opcao == 'ncc':
+            criar_conta_corrente(AGENCIA, contas_correntes, clientes)
 
-    elif opcao == 's':
-        valor = float(input('Informe o valor do saque: '))
-        novo_saldo, novo_extrato, novo_n_saques = sacar(
-            saldo=saldo, valor=valor, extrato=extrato,
-            limite=limite, numero_saques=numero_saques,
-            limite_saques=LIMITE_SAQUES
-        )
-        if novo_saldo and novo_extrato and novo_n_saques:
-            saldo, extrato, numero_saques = (
-                novo_saldo, novo_extrato, novo_n_saques
+        elif opcao == 'lc':
+            listar_contas_corrente(contas_correntes)
+
+        elif opcao == 'd':
+            valor = float(input('Informe o valor do depósito: '))
+            saldo, extrato = depositar(saldo, valor, extrato)
+
+        elif opcao == 's':
+            valor = float(input('Informe o valor do saque: '))
+            saldo, extrato, numero_saques = sacar(
+                saldo=saldo, valor=valor, extrato=extrato,
+                limite=limite, numero_saques=numero_saques,
+                limite_saques=LIMITE_SAQUES
             )
 
-    elif opcao == 'e':
-        novo_extrato = exibir_extrato(saldo, extrato=extrato)
-        if novo_extrato:
-            extrato = novo_extrato
+        elif opcao == 'e':
+            exibir_extrato(saldo, extrato=extrato)
 
-    elif opcao == 'q':
-        break
+        elif opcao == 'q':
+            break
 
-    else:
-        print(
-            'Operação inválida, '
-            'por favor selecione novamente a operação desejada.'
-        )
+        else:
+            print(
+                'Operação inválida, '
+                'por favor selecione novamente a operação desejada.'
+            )
+
+
+main()

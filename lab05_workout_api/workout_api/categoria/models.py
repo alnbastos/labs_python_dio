557aa262339e85a712c4dcdff80eb3ddfe96eb06
Aline Bastos <alinebastos23@outlook.com>
2026-01-01 11:07:57 -0300

    fix: adiciona unicidade aos campos dos modelos de atleta, categoria e ct

--- lab05_workout_api/workout_api/categoria/models.py
+++ lab05_workout_api/workout_api/categoria/models.py
@@ -10,7 +10,7 @@ class CategoriaModel(BaseModel):
     __tablename__ = "categorias"
 
     pk_id: Mapped[int] = mapped_column(Integer, primary_key=True)
-    nome: Mapped[str] = mapped_column(String(10), nullable=False)
+    nome: Mapped[str] = mapped_column(String(10), unique=True, nullable=False)
     criado_em: Mapped[datetime] = mapped_column(DateTime, nullable=False)
 
     atleta = Mapped['AtletaModel'] = relationship(back_populates='categoria')

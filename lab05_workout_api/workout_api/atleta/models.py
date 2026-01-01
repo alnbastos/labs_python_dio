2af0021c7ab875b06ef7da4bf1e6fa150cd6750f
Aline Bastos <alinebastos23@outlook.com>
2026-01-01 10:56:35 -0300

    feat: adiciona models e schemas de atleta

---
+++ lab05_workout_api/workout_api/atleta/models.py
@@ -0,0 +1,19 @@
+from datetime import datetime
+
+from sqlalchemy import DateTime, Float, Integer, String
+from sqlalchemy.orm import Mapped, mapped_column
+
+from contrib.models import BaseModel
+
+
+class AtletlaModel(BaseModel):
+    __tablename__ = "atletas"
+
+    pk_id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    nome: Mapped[str] = mapped_column(String(50), nullable=False)
+    cpf: Mapped[str] = mapped_column(String(11), nullable=False)
+    idade: Mapped[int] = mapped_column(Integer, nullable=False)
+    peso: Mapped[float] = mapped_column(Float, nullable=False)
+    altura: Mapped[float] = mapped_column(Float, nullable=False)
+    sexo: Mapped[str] = mapped_column(String(1), nullable=False)
+    criado_em: Mapped[datetime] = mapped_column(DateTime, nullable=False)

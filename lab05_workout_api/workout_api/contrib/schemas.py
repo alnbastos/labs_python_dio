eeeb596dfd34d85f7750e05d9672198e9cb454c8
Aline Bastos <alinebastos23@outlook.com>
2026-01-01 23:48:06 -0300

    feat: adiciona tipo de sessão do banco de dados e schema padrão de saída

--- lab05_workout_api/workout_api/contrib/schemas.py
+++ lab05_workout_api/workout_api/contrib/schemas.py
@@ -1,7 +1,14 @@
-from pydantic import BaseModel
+from datetime import datetime
+
+from pydantic import UUID4, BaseModel
 
 
 class BaseSchema(BaseModel):
     class Config:
-        extra = 'forbid'
+        extra = "forbid"
         from_attributes = True
+
+
+class OutMixin(BaseSchema):
+    id: UUID4
+    criado_em: datetime
